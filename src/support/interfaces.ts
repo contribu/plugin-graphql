d444064bd7d0ffab01e353e8592aabd8d7778378
mac2 <mac2@example.com>
2020-01-17 10:16:31 +0900

    add build query hook

--- src/support/interfaces.ts
+++ src/support/interfaces.ts
@@ -36,6 +36,8 @@ export interface ActionParams {
   query?: string | DocumentNode;
   multiple?: boolean;
   name?: string;
+  beforeBuildQuery?: any;
+  afterBuildQuery?: any;
 }
 
 export interface Data extends ORMInstance<PatchedModel> {
