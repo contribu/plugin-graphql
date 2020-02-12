54a18d7a269fb6e07df83867ce92e3ab2f8939cd
mac2 <mac2@example.com>
2020-02-12 10:37:53 +0900

    remove es6 build

--- dist/lib_es6/index.js
+++
@@ -1,4 +0,0 @@
-import VuexORMGraphQLPlugin from "./plugin";
-export default VuexORMGraphQLPlugin;
-export { setupTestUtils, mock, clearORMStore, Mock } from "./test-utils";
-//# sourceMappingURL=index.js.map
